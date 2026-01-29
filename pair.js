a8f9f00c6d4e69de1b7f1d38bd2dc5c31da5096e
Sanithu83 <sanithugeesath815@gmail.com>
2026-01-29 17:42:33 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://chatgpt.com/backend-api/estuary/content?id=file_00000000801071fd8dd74f79c5eedbdf&ts=491580&p=fs&cid=1&sig=80a75a0908bcf72ed24291160d03385c0531b7e3c3e39a2f71668658aa369dce&v=0",
               },
               caption: sid,
             });
@@ -129,4 +129,5 @@ process.on("uncaughtException", function (err) {
 });
 
 module.exports = router;
+
 
